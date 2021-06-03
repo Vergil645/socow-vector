b40daa0d338334b5ea180d7fa04e780a4461c69a
Vergil645 <kolesov645@gmail.com>
2021-06-03 21:35:34 +0300

    tests passed

--- socow-vector.h
+++ socow-vector.h
@@ -6,7 +6,7 @@ struct socow_vector {
     using iterator = T*;
     using const_iterator = T const*;
 
-    socow_vector() noexcept : _stat_buffer() {}
+    socow_vector() : _stat_buffer() {}
 
     socow_vector(socow_vector const& other) : socow_vector() {
         _size = other._size;
@@ -14,7 +14,7 @@ struct socow_vector {
             _stat_buffer = other._stat_buffer;
         } else {
             _is_small = false;
-            destruct(_stat_buffer);
+            // destruct(_stat_buffer);
             _dyn_buffer = other._dyn_buffer;
             _dyn_buffer->_copy_count++;
         }
@@ -25,7 +25,7 @@ struct socow_vector {
         return *this;
     }
 
-    ~socow_vector() noexcept {
+    ~socow_vector() {
         if (_is_small) {
             clear_prefix(_stat_buffer, _size);
         } else {
@@ -38,67 +38,89 @@ struct socow_vector {
         }
     }
 
-    T& operator[](size_t i) noexcept {
+    T& operator[](size_t i) {
+        if (!_is_small && _dyn_buffer->_copy_count != 1) {
+            change_capacity(capacity());
+        }
         return _is_small ? *reinterpret_cast<T*>(&_stat_buffer[i])
                          : _dyn_buffer->_data[i];
     }
 
-    T const& operator[](size_t i) const noexcept {
+    T const& operator[](size_t i) const {
         return _is_small ? *reinterpret_cast<T const*>(&_stat_buffer[i])
                          : _dyn_buffer->_data[i];
     }
 
-    T* data() noexcept {
+    T* data() {
+        if (!_is_small && _dyn_buffer->_copy_count != 1) {
+            change_capacity(capacity());
+        }
         return _is_small ? reinterpret_cast<T*>(&_stat_buffer[0])
                          : _dyn_buffer->_data;
     }
 
-    T const* data() const noexcept {
+    T const* data() const {
         return _is_small ? reinterpret_cast<T const*>(&_stat_buffer[0])
                          : _dyn_buffer->_data;
     }
 
-    size_t size() const noexcept {
+    size_t size() const {
         return _size;
     }
 
-    T& front() noexcept {
+    T& front() {
+        if (!_is_small && _dyn_buffer->_copy_count != 1) {
+            change_capacity(capacity());
+        }
         return _is_small ? *reinterpret_cast<T*>(&_stat_buffer[0])
                          : _dyn_buffer->_data[0];
     }
 
-    T const& front() const noexcept {
+    T const& front() const {
         return _is_small ? *reinterpret_cast<T const*>(&_stat_buffer[0])
                          : _dyn_buffer->_data[0];
     }
 
-    T& back() noexcept {
+    T& back() {
+        if (!_is_small && _dyn_buffer->_copy_count != 1) {
+            change_capacity(capacity());
+        }
         return _is_small ? *reinterpret_cast<T*>(&_stat_buffer[_size - 1])
                          : _dyn_buffer->_data[_size - 1];
     }
 
-    T const& back() const noexcept {
+    T const& back() const {
         return _is_small ? *reinterpret_cast<T const*>(&_stat_buffer[_size - 1])
                          : _dyn_buffer->_data[_size - 1];
     }
 
     void push_back(T const& value) {
         if (_size == capacity()) {
-            size_t val_pos = &value >= begin()
-                               ? &value - begin()
+            size_t val_pos = &value >= get_begin()
+                               ? &value - get_begin()
                                : std::numeric_limits<size_t>::max();
             change_capacity(capacity() * 2);
-            new (end())
+            new (get_end())
                 T(val_pos < _size ? _dyn_buffer->_data[val_pos] : value);
         } else if (_is_small) {
             new (&_stat_buffer[_size]) T(value);
         } else {
-            new (end()) T(value);
+            size_t val_pos = &value >= get_begin()
+                               ? &value - get_begin()
+                               : std::numeric_limits<size_t>::max();
+            if (_dyn_buffer->_copy_count != 1) {
+                change_capacity(capacity());
+            }
+            new (get_end())
+                T(val_pos < _size ? _dyn_buffer->_data[val_pos] : value);
         }
         _size++;
     }
 
-    void pop_back() noexcept {
+    void pop_back() {
+        if (!_is_small && _dyn_buffer->_copy_count != 1) {
+            change_capacity(capacity());
+        }
         _size--;
         if (_is_small) {
             reinterpret_cast<T*>(&_stat_buffer[_size])->~T();
@@ -107,17 +129,19 @@ struct socow_vector {
         }
     }
 
-    bool empty() const noexcept {
+    bool empty() const {
         return _size == 0;
     }
 
-    size_t capacity() const noexcept {
+    size_t capacity() const {
         return _is_small ? SMALL_SIZE : _dyn_buffer->_capacity;
     }
 
     void reserve(size_t new_cap) {
         if (capacity() < new_cap) {
             change_capacity(new_cap);
+        } else if (!_is_small && _dyn_buffer->_copy_count != 1) {
+            change_capacity(capacity());
         }
     }
 
@@ -127,50 +151,95 @@ struct socow_vector {
         }
     }
 
-    void clear() noexcept {
+    void clear() {
         if (_is_small) {
             clear_prefix(_stat_buffer, _size);
         } else {
+            if (_dyn_buffer->_copy_count != 1) {
+                change_capacity(capacity());
+            }
             clear_prefix(*_dyn_buffer, _size);
         }
         _size = 0;
     }
 
     void swap(socow_vector& other) {
-        if (capacity() < other.capacity()) {
-            change_capacity(other.capacity());
-        } else if (capacity() > other.capacity()) {
-            other.change_capacity(capacity());
-        }
-        std::swap(_size, other._size);
         if (_is_small) {
-            std::swap(_stat_buffer, other._stat_buffer);
+            if (other._is_small) {
+                std::array<std::aligned_storage_t<sizeof(T), alignof(T)>,
+                    SMALL_SIZE>
+                    tmp;
+                for (size_t i = 0; i != _size; i++) {
+                    try {
+                        new (&tmp[i]) T(*reinterpret_cast<T*>(&_stat_buffer[i]));
+                    } catch (...) {
+                        clear_prefix(tmp, i);
+                        throw;
+                    }
+                }
+                clear_prefix(_stat_buffer, _size);
+                for (size_t i = 0; i != other._size; i++) {
+                    try {
+                        new (&_stat_buffer[i])
+                            T(*reinterpret_cast<T*>(&other._stat_buffer[i]));
+                    } catch (...) {
+                        clear_prefix(tmp, _size);
+                        throw;
+                    }
+                }
+                clear_prefix(other._stat_buffer, other._size);
+                for (size_t i = 0; i != _size; i++) {
+                    try {
+                        new (&other._stat_buffer[i])
+                            T(*reinterpret_cast<T*>(&tmp[i]));
+                    } catch (...) {
+                        clear_prefix(tmp, _size);
+                        throw;
+                    }
+                }
+                clear_prefix(tmp, _size);
+                std::swap(_size, other._size);
+            } else {
+                change_capacity(other.capacity());
+                std::swap(_size, other._size);
+                std::swap(_dyn_buffer, other._dyn_buffer);
+            }
         } else {
-            std::swap(_dyn_buffer, other._dyn_buffer);
+            if (other._is_small) {
+                other.change_capacity(capacity());
+                std::swap(_size, other._size);
+                std::swap(_dyn_buffer, other._dyn_buffer);
+            } else {
+                std::swap(_size, other._size);
+                std::swap(_dyn_buffer, other._dyn_buffer);
+            }
         }
     }
 
-    iterator begin() noexcept {
+    iterator begin() {
+        if (!_is_small && _dyn_buffer->_copy_count != 1) {
+            change_capacity(capacity());
+        }
         return _is_small ? reinterpret_cast<T*>(&_stat_buffer[0])
                          : _dyn_buffer->_data;
     }
 
-    iterator end() noexcept {
+    iterator end() {
         return begin() + _size;
     }
 
-    const_iterator begin() const noexcept {
+    const_iterator begin() const {
         return _is_small ? reinterpret_cast<T const*>(&_stat_buffer[0])
                          : _dyn_buffer->_data;
     }
 
-    const_iterator end() const noexcept {
+    const_iterator end() const {
         return _is_small ? reinterpret_cast<T const*>(&_stat_buffer[0]) + _size
-                         : _dyn_buffer->_data;
+                         : _dyn_buffer->_data + _size;
     }
 
     iterator insert(const_iterator pos, T const& value) {
-        size_t pos_index = pos - begin();
+        size_t pos_index = pos - get_begin();
         push_back(value);
         if (_is_small) {
             for (size_t i = _size - 1; i != pos_index; i--) {
@@ -181,19 +250,22 @@ struct socow_vector {
                 std::swap(_dyn_buffer->_data[i - 1], _dyn_buffer->_data[i]);
             }
         }
-        return begin() + pos_index;
+        return get_begin() + pos_index;
     }
 
-    iterator erase(const_iterator pos) noexcept {
-        return pos == end() ? end() : erase(pos, pos + 1);
+    iterator erase(const_iterator pos) {
+        return pos == get_end() ? end() : erase(pos, pos + 1);
     }
 
-    iterator erase(const_iterator first, const_iterator last) noexcept {
-        if (first == last) {
-            return const_cast<iterator>(first);
+    iterator erase(const_iterator first, const_iterator last) {
+        size_t first_index = first - get_begin();
+        size_t count = last - first;
+        if (!_is_small && _dyn_buffer->_copy_count != 1) {
+            change_capacity(capacity());
+        }
+        if (count == 0) {
+            return const_cast<iterator>(get_begin() + first_index);
         } else {
-            size_t first_index = first - begin();
-            size_t count = last - first;
             if (_is_small) {
                 for (size_t i = first_index; i != _size - count; i++) {
                     std::swap(_stat_buffer[i], _stat_buffer[i + count]);
@@ -207,13 +279,13 @@ struct socow_vector {
             for (size_t i = 0; i != count; i++) {
                 pop_back();
             }
-            return begin() + first_index;
+            return get_begin() + first_index;
         }
     }
 
 private:
     struct dynamic_storage {
-        dynamic_storage() noexcept : _data(nullptr) {}
+        dynamic_storage() : _data(nullptr) {}
 
         explicit dynamic_storage(size_t cap) : dynamic_storage() {
             _capacity = cap;
@@ -231,16 +303,27 @@ private:
 
     bool _is_small{true};
     size_t _size{0};
-//    std::array<std::aligned_storage_t<sizeof(T), alignof(T)>, SMALL_SIZE>
-//        _stat_buffer;
-//    dynamic_storage* _dyn_buffer;
     union {
         std::array<std::aligned_storage_t<sizeof(T), alignof(T)>, SMALL_SIZE>
             _stat_buffer;
         dynamic_storage* _dyn_buffer;
     };
 
-    template<typename S>
+    T& get_elem(size_t i) {
+        return _is_small ? *reinterpret_cast<T*>(&_stat_buffer[i])
+                         : _dyn_buffer->_data[i];
+    }
+
+    iterator get_begin() {
+        return _is_small ? reinterpret_cast<T*>(&_stat_buffer[0])
+                         : _dyn_buffer->_data;
+    }
+
+    iterator get_end() {
+        return get_begin() + _size;
+    }
+
+    template <typename S>
     static void destruct(S& obj) {
         obj.~S();
     }
@@ -268,7 +351,7 @@ private:
                 auto* new_dyn_buffer = new dynamic_storage(new_cap);
                 for (size_t i = 0; i != _size; i++) {
                     try {
-                        new (new_dyn_buffer->_data + i) T(operator[](i));
+                        new (new_dyn_buffer->_data + i) T(get_elem(i));
                     } catch (...) {
                         clear_prefix(*new_dyn_buffer, i);
                         delete new_dyn_buffer;
@@ -276,7 +359,6 @@ private:
                     }
                 }
                 clear_prefix(_stat_buffer, _size);
-                destruct(_stat_buffer);
                 _dyn_buffer = new_dyn_buffer;
                 _dyn_buffer->_copy_count++;
                 _is_small = false;
@@ -288,10 +370,9 @@ private:
                     new_stat_buffer;
                 for (size_t i = 0; i != _size; i++) {
                     try {
-                        new (&new_stat_buffer[i]) T(operator[](i));
+                        new (&new_stat_buffer[i]) T(get_elem(i));
                     } catch (...) {
                         clear_prefix(new_stat_buffer, i);
-                        new_stat_buffer.~array();
                         throw;
                     }
                 }
@@ -301,13 +382,23 @@ private:
                 } else {
                     _dyn_buffer->_copy_count--;
                 }
-                _stat_buffer = new_stat_buffer;
+                for (size_t i = 0; i != _size; i++) {
+                    try {
+                        new (&_stat_buffer[i])
+                            T(*reinterpret_cast<T*>(&new_stat_buffer[i]));
+                    } catch (...) {
+                        clear_prefix(_stat_buffer, i);
+                        clear_prefix(new_stat_buffer, _size);
+                        throw;
+                    }
+                }
+                clear_prefix(new_stat_buffer, _size);
                 _is_small = true;
             } else {
                 auto* new_dyn_buffer = new dynamic_storage(new_cap);
                 for (size_t i = 0; i != _size; i++) {
                     try {
-                        new (new_dyn_buffer->_data + i) T(operator[](i));
+                        new (new_dyn_buffer->_data + i) T(get_elem(i));
                     } catch (...) {
                         clear_prefix(*new_dyn_buffer, i);
                         delete new_dyn_buffer;
